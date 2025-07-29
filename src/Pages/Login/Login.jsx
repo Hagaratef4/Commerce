1726115095cd9bbc27a6291227f984c18806628f
Hagar <tgyg734@gmail.com>
2025-07-29 06:06:48 +0300

    .new

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -8,7 +8,8 @@ import { authContext } from '../../Context/AuthContext';
 
 function Login() {
 
-    const passReg = /^[A-Z][A-Za-z0-9]{5,}$/
+    const passReg = /^.{5,}$/
+
     const [showPass , setShowPass] = useState('password')
     const [err,setErr] = useState('')
     let {setToken, verifyToken}= useContext(authContext)
@@ -21,7 +22,7 @@ function Login() {
 
     const validationSchema = yup.object({
         email :yup.string().required('email must required').email('must be email'),
-        password :yup.string().required('password is requried').matches(passReg,'enter the password'),
+        password :yup.string().required('password is requried').matches(passReg,'enter the correct password'),
     })
 
     const formik = useFormik({
@@ -89,18 +90,16 @@ function Login() {
                                 )}
 
                             {/* password */}
-                        <div className='relative'>
                             <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+                        <div className='relative'>
                             <input 
                                 name = 'password'
                                 value={formik.values.password}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 type={showPass} 
-                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full " />
-                        </div>
-
-                            <div className='absolute top-[46%] right-[22%] md:top-[50.5%] md:right-[38.5%] xl:top-[47%]' onClick={toggleShowPass}>
+                            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full pr-10 " />
+                            <div className="absolute right-3 top-1/3 -translate-y-1/2 cursor-pointer text-gray-900" onClick={toggleShowPass}>
                                 {showPass === 'password' ?
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
@@ -110,8 +109,6 @@ function Login() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>}
-
-
                             </div>
                             {formik.errors.password && formik.touched.password && ( 
                                     <p className='bg-red-300 p-2 rounded-2xl mb-3'>
@@ -119,6 +116,9 @@ function Login() {
                                     </p>
                                     
                             )}
+                        </div>
+
+                            
 
                             <button type='submit' class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                                 <span class="inline-block mr-2">Sing in</span>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -7,7 +7,7 @@ import * as yup from 'yup';
 
 function Register() {
 
-    const passReg = /^[A-Z][A-Za-z0-9]{5,}$/
+    const passReg = /^.{5,}$/
     const phoneReg = /^01[0125][0-9]{8}$/
     const [showPass , setShowPass] = useState('password')
 
@@ -20,7 +20,7 @@ function Register() {
     const validationSchema = yup.object({
         name :yup.string().required('name must required').min(4,'minimum name is 4 chars').max(20,'maximum name is 20 chars'),
         email :yup.string().required('email must required').email('must be email'),
-        password :yup.string().required('password is requried').matches(passReg,'enter the password'),
+        password :yup.string().required('password is requried').matches(passReg,'wrong password'),
         rePassword : yup.string().required('re-password is required').oneOf([yup.ref('password')],'must be match the password'),
         phone:yup.string().required('phone is required').matches(phoneReg,'enter the phone') 
     })
@@ -102,18 +102,35 @@ function Register() {
 
                                 {/* password */}
                                 <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+                                <div className="relative">
                                 <input 
                                     name = 'password'
                                     value={formik.values.password}
                                     onChange={formik.handleChange}
                                     type={showPass} 
-                                class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full pr-10" />
+                                <div
+                                        className="absolute right-3 top-1/3 -translate-y-1/2 cursor-pointer text-gray-900"
+                                        onClick={toggleShowPass}
+                                        >
+                                    {showPass === 'password' ?
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                    </svg>
+                                                                    
+                                                                    :<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>}
+                                </div>
                                 {formik.errors.password && formik.touched.password && ( 
                                         <p className='bg-red-300 p-2 rounded-2xl mb-3'>
                                             {formik.errors.password}
                                         </p>
                                         
                                 )}
+                                </div>
+
 
                                 {/* re password */}
                                 <div>
@@ -124,21 +141,9 @@ function Register() {
                                         onChange={formik.handleChange}
                                         onBlur={formik.handleBlur}
                                         type={showPass} 
-                                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                                        className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
                                 </div>
-                                <div className='absolute top-[69%] right-[15%] md:top-[75%] md:right-[30%] lg:top-[72%] xl:top-[69%] lg:right-[36%] xl:right-[39%] ' onClick={toggleShowPass}>
-                                    {showPass === 'password' ?
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                                    </svg>
-                                                                    
-                                    :<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    </svg>}
-
-
-                                </div>
+                                
 
                                 {formik.errors.rePassword && formik.touched.rePassword && ( 
                                         <p className='bg-red-300 p-2 rounded-2xl mb-3'>
